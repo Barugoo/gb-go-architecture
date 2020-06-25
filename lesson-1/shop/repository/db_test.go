d7b646c275efee3fc91ba5a679f2a03585d5b0ce
kaatinga <kaatinga@gmail.com>
2020-06-25 21:16:34 +0300

    unit test was added

--- lesson-1/shop/repository/db_test.go
+++ lesson-1/shop/repository/db_test.go
@@ -4,7 +4,7 @@ import (
 	"reflect"
 	"testing"
 
-	"../models"
+	"github.com/kaatinga/testModel"
 )
 
 func TestNewMapDB(t *testing.T) {
@@ -24,33 +24,30 @@ func TestNewMapDB(t *testing.T) {
 }
 
 func Test_mapDB_CreateItem(t *testing.T) {
-	type fields struct {
-		db    map[int32]*models.Item
-		maxID int32
-	}
-	type args struct {
-		item *models.Item
-	}
+
+	var inputItem models.Item
+	outputItem := models.Item{ID: 1}
+
 	tests := []struct {
 		name    string
-		fields  fields
-		args    args
+		object  mapDB
+		arg     *models.Item
 		want    *models.Item
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{"ok", mapDB{db: make(map[int32]*models.Item), maxID: 0}, &inputItem, &outputItem, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			m := &mapDB{
-				db:    tt.fields.db,
-				maxID: tt.fields.maxID,
-			}
-			got, err := m.CreateItem(tt.args.item)
+
+			m := &tt.object
+
+			got, err := m.CreateItem(tt.arg)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("CreateItem() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
+
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("CreateItem() got = %v, want %v", got, tt.want)
 			}
@@ -58,86 +55,95 @@ func Test_mapDB_CreateItem(t *testing.T) {
 	}
 }
 
-func Test_mapDB_DeleteItem(t *testing.T) {
-	type fields struct {
-		db    map[int32]*models.Item
-		maxID int32
-	}
-	type args struct {
-		ID int32
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			m := &mapDB{
-				db:    tt.fields.db,
-				maxID: tt.fields.maxID,
-			}
-			if err := m.DeleteItem(tt.args.ID); (err != nil) != tt.wantErr {
-				t.Errorf("DeleteItem() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
-}
-
-func Test_mapDB_GetItem(t *testing.T) {
-	type fields struct {
-		db    map[int32]*models.Item
-		maxID int32
-	}
-	type args struct {
-		ID int32
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		want    *models.Item
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			m := &mapDB{
-				db:    tt.fields.db,
-				maxID: tt.fields.maxID,
-			}
-			got, err := m.GetItem(tt.args.ID)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("GetItem() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("GetItem() got = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
+//func Test_mapDB_DeleteItem(t *testing.T) {
+//	type fields struct {
+//		db    map[int32]*models.Item
+//		maxID int32
+//	}
+//	type args struct {
+//		ID int32
+//	}
+//	tests := []struct {
+//		name    string
+//		fields  fields
+//		args    args
+//		wantErr bool
+//	}{
+//		// TODO: Add test cases.
+//	}
+//	for _, tt := range tests {
+//		t.Run(tt.name, func(t *testing.T) {
+//			m := &mapDB{
+//				db:    tt.fields.db,
+//				maxID: tt.fields.maxID,
+//			}
+//			if err := m.DeleteItem(tt.args.ID); (err != nil) != tt.wantErr {
+//				t.Errorf("DeleteItem() error = %v, wantErr %v", err, tt.wantErr)
+//			}
+//		})
+//	}
+//}
+//
+//func Test_mapDB_GetItem(t *testing.T) {
+//	type fields struct {
+//		db    map[int32]*models.Item
+//		maxID int32
+//	}
+//	type args struct {
+//		ID int32
+//	}
+//	tests := []struct {
+//		name    string
+//		fields  fields
+//		args    args
+//		want    *models.Item
+//		wantErr bool
+//	}{
+//		// TODO: Add test cases.
+//	}
+//	for _, tt := range tests {
+//		t.Run(tt.name, func(t *testing.T) {
+//			m := &mapDB{
+//				db:    tt.fields.db,
+//				maxID: tt.fields.maxID,
+//			}
+//			got, err := m.GetItem(tt.args.ID)
+//			if (err != nil) != tt.wantErr {
+//				t.Errorf("GetItem() error = %v, wantErr %v", err, tt.wantErr)
+//				return
+//			}
+//			if !reflect.DeepEqual(got, tt.want) {
+//				t.Errorf("GetItem() got = %v, want %v", got, tt.want)
+//			}
+//		})
+//	}
+//}
+//
 func Test_mapDB_UpdateItem(t *testing.T) {
-	type fields struct {
-		db    map[int32]*models.Item
-		maxID int32
-	}
-	type args struct {
-		item *models.Item
-	}
+
+	var testItem models.Item
+	testItem.ID = 1
+	testItem.Name = "test"
+	testItem.Price = 100
+
+	var incorrectItem models.Item
+	incorrectItem.ID = 2
+
+	var fields mapDB
+	fields.db = make(map[int32]*models.Item)
+	fields.maxID = 1
+
+	fields.db[1] = &testItem
+
 	tests := []struct {
 		name    string
-		fields  fields
-		args    args
+		fields  mapDB
+		args    *models.Item
 		want    *models.Item
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{"ok", fields, &testItem, &testItem, false},
+		{"wrong", fields, &incorrectItem, nil, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -145,7 +151,7 @@ func Test_mapDB_UpdateItem(t *testing.T) {
 				db:    tt.fields.db,
 				maxID: tt.fields.maxID,
 			}
-			got, err := m.UpdateItem(tt.args.item)
+			got, err := m.UpdateItem(tt.args)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("UpdateItem() error = %v, wantErr %v", err, tt.wantErr)
 				return
