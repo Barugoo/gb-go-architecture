d740166fcc749f0149c2544286271daa1f85591b
Denis Bulavin <admin@kuznitsa.pro>
2021-01-25 13:35:48 +0300

    fix

--- lesson-3/main.go
+++ lesson-3/main.go
@@ -8,7 +8,7 @@ import (
 	"time"
 )
 
-var count float64
+var timeAll float64
 var goodResp float64
 
 func httpGet(url string) (int, float64) {
@@ -45,19 +45,22 @@ func worker(w int, url string, jobs <-chan int, results chan<- int, method strin
 			code, t := httpGet(url)
 			if code >= 200 && code <= 299 {
 				fmt.Println("Время отклика: ", t)
-				count = +t
 				goodResp++
 			}
+			timeAll += t
 
 			results <- j * 2
 		}
-	} else {
+	}
+	if method == "POST" {
 		for j := range jobs {
 			code, t := httpPost(url, body)
 			if code >= 200 && code <= 299 {
 				fmt.Println("Время отклика: ", t)
-				count = +t
+
+				goodResp++
 			}
+			timeAll += t
 
 			results <- j * 2
 		}
@@ -70,35 +73,52 @@ func main() {
 	var url string
 	var workerCount int
 	var jobCount int
+	var leadTime time.Duration
 	var httpMethod string
 	var bodyReques []byte
 
 	flag.StringVar(&url, "url", "https://yandex.ru", "URL сайта")
-	flag.IntVar(&workerCount, "w", 5, "number of workers")
-	flag.IntVar(&jobCount, "j", 5, "number of jobs")
+	flag.IntVar(&workerCount, "t", 5, "number of threads")
+	flag.IntVar(&jobCount, "r", 5, "number of requests")
+	flag.DurationVar(&leadTime, "t", 0, "lead time")
 	flag.StringVar(&httpMethod, "m", "GET", "method GET or POST")
 
 	flag.Parse()
 
+	if httpMethod != "GET" && httpMethod != "POST" {
+		panic("Вы ввели не верный метод. Разрешенный метод \"GET\" или \"POST\"")
+	}
+
+	if workerCount <= 0 {
+		panic("Колличество потоков должно быть больше 0")
+	}
+
+	if jobCount <= 0 {
+		panic("Колличество запросов должно быть больше 0")
+	}
+
+	if leadTime < 0 {
+		panic("Время не может быть отрицательное")
+	}
+
 	jobs := make(chan int, jobCount)
 	results := make(chan int, jobCount)
 
-	for w := 1; w <= workerCount; w++ {
+	for w := 0; w < workerCount; w++ {
 		go worker(w, url, jobs, results, httpMethod, bodyReques)
 	}
 
-	for j := 1; j <= jobCount; j++ {
+	for j := 0; j < jobCount; j++ {
 		jobs <- j
 	}
 	close(jobs)
 
-	for a := 1; a <= jobCount; a++ {
+	for a := 0; a < jobCount; a++ {
 		<-results
 	}
 
-	rpc := goodResp / count
-	sred := count / goodResp
+	rpc := timeAll / goodResp
+
 	fmt.Println("RPC: ", rpc)
-	fmt.Println("Среднее значение: ", sred)
 
 }
